d54c1610679808e8c8caf9c116e96768f6c87ef9
tiamo.405 <nam05052002@gmail.com>
2025-06-14 10:59:13 +0700

    init

--- create-new-container.sh
+++ create-new-container.sh
@@ -1,4 +1,4 @@
-lxc launch ubuntu:22.04 name-container \
+lxc launch ubuntu:22.04 lab-server-1 \
   --network br0 \
   --config=user.user-data="$(cat cloud-init-namtp.yaml)" \
   --config=user.network-config="$(cat cloud-init-network.yaml)"
